5cc0b2bc6101de25be04ef7682833b8c1293f6d7
Thabo <tdmphuthi@gmail.com>
2019-09-29 19:24:36 +0200

    add todo list view and addition support

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoListComponent } from './todo-list.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
@@ -8,7 +9,8 @@ describe('TodoListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
+      declarations: [ TodoListComponent ],
+      imports: [ReactiveFormsModule]
     })
     .compileComponents();
   }));
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  private todoList: string[];
+  private isAdding = false;
+  newItem = new FormControl('');
 
   constructor() { }
 
   ngOnInit() {
+    // Read from local storage
+    this.todoList = JSON.parse(window.localStorage.getItem('todo-list')) || [];
   }
 
+  add(item) {
+    this.todoList.push(item);
+    this.isAdding = false;
+    this.newItem.reset();
+
+    // Write to local storage
+    window.localStorage.setItem('todo-list', JSON.stringify(this.todoList));
+  }
 }
